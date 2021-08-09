dbbf26f94969653789c6972df1cdf8f15506fc1a
Omar <omar.branez@gmail.com>
2021-08-09 15:38:13 -0400

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeHearts = document.querySelectorAll(".like-glyph")
 
+function likeAction(e) { //e is the target
+  const heartSymbol = e.target
+  // debugger;
+  mimicServerCall("lol")
+    .then(function(serverMessage){
+      if (e.target.innerHTML == EMPTY_HEART) {
+      heartSymbol.innerText = FULL_HEART
+      heartSymbol.setAttribute("class", "activated-heart")
+    }
+      else {
+        heartSymbol.innerText = EMPTY_HEART
+        heartSymbol.className = ""
+        heartSymbol.setAttribute("class", "like-glyph")
+      }})
+    .catch(function(error) {
+      const modal = document.getElementById("modal")
+      modal.className = ""
+      modal.innerText = error
+      setTimeout( () => modal.className = "hidden", 3000)
+    })
+}
 
+for (const glyph of likeHearts) { //iterate over all the likeHearts
+  glyph.addEventListener("click", likeAction);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
